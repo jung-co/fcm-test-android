8a3011870ca74b7f1f518942b95acd2143d8a9bd
HanJungYoon <fpqpf02@kyobobook.com>
2021-12-14 17:42:09 +0900

    only first push message

--- app/src/main/java/com/jungco/fcmtestapp/MyFirebaseMessagingService.java
+++ app/src/main/java/com/jungco/fcmtestapp/MyFirebaseMessagingService.java
@@ -21,75 +21,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = "MyFirebaseMsgService";
 
-    /**
-     * 메시지 수신받는 메소드
-     * @param msg
-     */
-    @Override
-    public void onMessageReceived(RemoteMessage msg) {
-        Log.i("### msg : ", msg.toString());
-
-        if (msg.getData().size() > 0) {
-            Log.i("### data : ", msg.getData().toString());
-            sendTopNotification(msg.getData().get("title"), msg.getData().get("content"));
-            if (true) {
-                scheduleJob();
-            } else {
-                handleNow();
-            }
-        }
-
-        if (msg.getNotification() != null) {
-            Log.i("### notification : ", msg.getNotification().getBody());
-            sendTopNotification(msg.getNotification().getTitle(), msg.getNotification().getBody());
-        }
-    }
-
-    /**
-     * Schedule async work using WorkManager.
-     */
-    private void scheduleJob() {
-        OneTimeWorkRequest work = new OneTimeWorkRequest.Builder(MyWorker.class)
-                .build();
-        WorkManager.getInstance().beginWith(work).enqueue();
-    }
-
-    /**
-     * Handle time allotted to BroadcastReceivers.
-     */
-    private void handleNow() {
-        Log.d(TAG, "Short lived task is done.");
-    }
-
-    private void sendTopNotification(String title, String content) {
-        final String CHANNEL_DEFAULT_IMPORTANCE = "channel_id";
-        final int ONGOING_NOTIFICATION_ID = 1;
-        Intent notificationIntent = new Intent(this, MyFirebaseMessagingService.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
-
-        Notification notification =
-                new Notification.Builder(this, CHANNEL_DEFAULT_IMPORTANCE)
-                        .setContentTitle(title)
-                        .setContentText(content)
-                        .setSmallIcon(R.drawable.common_full_open_on_phone)
-                        .setContentIntent(pendingIntent)
-                        .build();
-
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        // Since android Oreo notification channel is needed.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(CHANNEL_DEFAULT_IMPORTANCE,
-                    "Channel human readable title",
-                    NotificationManager.IMPORTANCE_DEFAULT);
-            notificationManager.createNotificationChannel(channel);
-        }
-
-        notificationManager.notify(ONGOING_NOTIFICATION_ID, notification);
-    }
-
-/*
     public void showDataMessage(String msgTitle, String msgContent) {
         Log.i("### data msgTitle : ", msgTitle);
         Log.i("### data msgContent : ", msgContent);
@@ -103,6 +34,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
      * 수신받은 메시지를 Toast로 보여줌
      * @param msgTitle
      * @param msgContent
+     */
 
     public void showNotificationMessage(String msgTitle, String msgContent) {
         Log.i("### noti msgTitle : ", msgTitle);
@@ -116,6 +48,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     /**
      * 메시지 수신받는 메소드
      * @param msg
+     */
 
     @Override
     public void onMessageReceived(RemoteMessage msg) {
@@ -126,5 +59,4 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             showDataMessage(msg.getData().get("title"), msg.getData().get("content"));  // Data로 받을 때
         }
     }
- */
 }

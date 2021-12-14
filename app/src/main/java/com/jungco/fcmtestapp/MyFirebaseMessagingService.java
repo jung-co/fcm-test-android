ef601ec8e6e4033f12f229a608b2e6b8cdaf299e
HanJungYoon <fpqpf02@kyobobook.com>
2021-12-14 17:44:03 +0900

    Fixed Error : Service took too long to process intent

--- app/src/main/java/com/jungco/fcmtestapp/MyFirebaseMessagingService.java
+++ app/src/main/java/com/jungco/fcmtestapp/MyFirebaseMessagingService.java
@@ -21,42 +21,71 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = "MyFirebaseMsgService";
 
-    public void showDataMessage(String msgTitle, String msgContent) {
-        Log.i("### data msgTitle : ", msgTitle);
-        Log.i("### data msgContent : ", msgContent);
-        String toastText = String.format("[Data 메시지] title: %s => content: %s", msgTitle, msgContent);
-        Looper.prepare();
-        Toast.makeText(getApplicationContext(), toastText, Toast.LENGTH_LONG).show();
-        Looper.loop();
-    }
-
-    /**
-     * 수신받은 메시지를 Toast로 보여줌
-     * @param msgTitle
-     * @param msgContent
-     */
-
-    public void showNotificationMessage(String msgTitle, String msgContent) {
-        Log.i("### noti msgTitle : ", msgTitle);
-        Log.i("### noti msgContent : ", msgContent);
-        String toastText = String.format("[Notification 메시지] title: %s => content: %s", msgTitle, msgContent);
-        Looper.prepare();
-        Toast.makeText(getApplicationContext(), toastText, Toast.LENGTH_LONG).show();
-        Looper.loop();
-    }
-
     /**
      * 메시지 수신받는 메소드
      * @param msg
      */
-
     @Override
     public void onMessageReceived(RemoteMessage msg) {
         Log.i("### msg : ", msg.toString());
-        if (msg.getData().isEmpty()) {
-            showNotificationMessage(msg.getNotification().getTitle(), msg.getNotification().getBody());  // Notification으로 받을 때
-        } else {
-            showDataMessage(msg.getData().get("title"), msg.getData().get("content"));  // Data로 받을 때
+
+        if (msg.getData().size() > 0) {
+            Log.i("### data : ", msg.getData().toString());
+            sendTopNotification(msg.getData().get("title"), msg.getData().get("content"));
+            if (true) {
+                scheduleJob();
+            } else {
+                handleNow();
+            }
         }
+
+        if (msg.getNotification() != null) {
+            Log.i("### notification : ", msg.getNotification().getBody());
+            sendTopNotification(msg.getNotification().getTitle(), msg.getNotification().getBody());
+        }
+    }
+
+    /**
+     * Schedule async work using WorkManager.
+     */
+    private void scheduleJob() {
+        OneTimeWorkRequest work = new OneTimeWorkRequest.Builder(MyWorker.class)
+                .build();
+        WorkManager.getInstance().beginWith(work).enqueue();
+    }
+
+    /**
+     * Handle time allotted to BroadcastReceivers.
+     */
+    private void handleNow() {
+        Log.d(TAG, "Short lived task is done.");
+    }
+
+    private void sendTopNotification(String title, String content) {
+        final String CHANNEL_DEFAULT_IMPORTANCE = "channel_id";
+        final int ONGOING_NOTIFICATION_ID = 1;
+        Intent notificationIntent = new Intent(this, MyFirebaseMessagingService.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        Notification notification =
+                new Notification.Builder(this, CHANNEL_DEFAULT_IMPORTANCE)
+                        .setContentTitle(title)
+                        .setContentText(content)
+                        .setSmallIcon(R.drawable.common_full_open_on_phone)
+                        .setContentIntent(pendingIntent)
+                        .build();
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_DEFAULT_IMPORTANCE,
+                    "Channel human readable title",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(ONGOING_NOTIFICATION_ID, notification);
     }
 }
